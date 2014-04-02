36e73f0680960f1f011662dd7004042ddf4493b0
MasaruFukuie <fukuie@level-five.jp>
2014-04-02 10:45:32 +0900

    first

---
+++ Calculator/src/test/java/CalcuratorTest.java
@@ -0,0 +1,19 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcuratorTest {
+
+	@Test
+	public void testCalcurator() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	public void testGetValue() {
+		fail("まだ実装されていません");
+	}
+
+}
